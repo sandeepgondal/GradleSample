7ec9dc977a1a602f84d8fffdf50ca2077613066b
Sandeep <sandeep.gondal@gmail.com>
2017-05-08 08:58:52 +0530

    Initial Commit

---
+++ src/main/java/com/sandy/MainClass.java
@@ -0,0 +1,11 @@
+package com.sandy;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MainClass {
+
+	public static void main(String [] args) {
+		System.out.println(StringUtils.swapCase("Hello Gradle"));
+	}
+
+}
